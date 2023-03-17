170d1b798e0908d02778d2ae58d60e1afee98629
AlIhtiarPeter <126416694+AlIhtiarPeter@users.noreply.github.com>
2023-03-17 19:14:16 +0200

    LI4-6: Added java files

---
+++ Forward Engineering/Java fles/Admin.java
@@ -0,0 +1,12 @@
+public class Admin extends Person {
+
+  public void getRaport(Patient patient) {
+  }
+
+  public void sendResults() {
+  }
+
+  public void updateAI() {
+  }
+
+}
---
+++ Forward Engineering/Java fles/Doctor.java
@@ -0,0 +1,47 @@
+import java.util.Vector;
+
+public class Doctor extends Person {
+
+  public String speciality;
+
+    /**
+   * 
+   * @element-type Patient
+   */
+  public Vector  patients;
+
+  public String getSpeciality() {
+  return null;
+  }
+
+  public Patient accesPatient(Patient pacient) {
+  return null;
+  }
+
+  public Patient declinePatient(Patient pacient) {
+  return null;
+  }
+
+  public void removePatient(Patient pacient) {
+  }
+
+  public Patient[] getPatientList() {
+  return null;
+  }
+
+  public void addDisease(Patient pacient, Disease disease) {
+  }
+
+  public void addSample(Sample sample) {
+  }
+
+  public void creadRaport(Patient pacient) {
+  }
+
+  public void getRaport(Patient pacient) {
+  }
+
+  public void checkResults(Patient pacient) {
+  }
+
+}
